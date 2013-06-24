7fcc3312781a47ad9215e4ab5090df618fb7b530
ronald manrique <ronald.manrique@gmail.com>
2013-06-24 13:33:42 -0500

    Implement features for displaying a list of relevant ickets for projects and viewing particular tickets

--- spec/support/factories/project_factory.rb
+++ spec/support/factories/project_factory.rb
@@ -2,4 +2,8 @@ FactoryGirl.define do
 	factory :project do
 		name "Example project" 
 	end
+	factory :ticket do
+		title "Example ticket"
+		description "An example ticket, nothing more"
+	end
 end
