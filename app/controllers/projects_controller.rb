f936817eccb06b0f99d6e39181e60d3f3958ff3b
ronald manrique <ronald.manrique@gmail.com>
2013-06-24 11:11:48 -0500

    redirect ...

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -47,5 +47,12 @@ class ProjectsController < ApplicationController
 		flash[:notice] = "Project has been deleted."
 		redirect_to projects_path
 	end
-	
+	private
+		def find_project
+			@project = Project.find(params[:id])
+			rescue ActiveRecord::RecordNotFound
+			flash[:alert] = "The project you were looking" +
+			" for could not be found."
+			redirect_to projects_path
+		end
 end
