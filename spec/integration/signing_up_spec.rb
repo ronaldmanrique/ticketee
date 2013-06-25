79ae724eb25e77b4f9fb96273dc58fd6e36391ae
ronald manrique <ronald.manrique@gmail.com>
2013-06-24 20:19:25 -0500

    Add feature for signing in via the Devise-provided form

--- spec/integration/signing_up_spec.rb
+++ spec/integration/signing_up_spec.rb
@@ -14,4 +14,13 @@ feature 'Signing in' do
 		message = "Please open the link to activate your account."
 		page.should have_content(message)
 	end
+	scenario 'Signing in via form' do
+		User.find_by_email('ticketee@example.com').confirm!
+		visit '/'
+		click_link 'Sign in'
+		fill_in 'Email', :with => "ticketee@example.com"
+		fill_in 'Password', :with => "password"
+		click_button "Sign in"
+		page.should have_content("Signed in successfully.")
+	end
 end
